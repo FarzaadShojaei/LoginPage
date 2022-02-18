ee74c030ca5a4d202df5b89b40c1244f7d979613
TheyCalledmeMrGlass <shojaeifarzad79@yahoo.com>
2022-02-19 01:01:50 +0330

    Adding HTML

---
+++ app.js
@@ -0,0 +1,34 @@
+const Koa = require('koa');
+const KoaRouter = require('koa-router');
+const json = require('koa-json');
+const app = new Koa();
+const router = new KoaRouter();
+const path = require('path');
+const render = require('koa-ejs');
+
+//Json Prettier Middleware
+app.use(json());
+
+//Simple Middleware Example
+//app.use(async ctx => ctx.body = { msg: 'Hello World' });
+
+router.get('/test', ctx => (ctx.body = 'Hello Test'));
+
+render(app, {
+    root: path.join(__dirname, 'views'),
+    layout: 'layout',
+    viewExt: 'html',
+    cache: false,
+    debug: false
+});
+
+//Index 
+router.get('/', async ctx => {
+    await ctx.render('index');
+})
+
+
+//Router Middleware
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000, () => console.log('server started..'));
